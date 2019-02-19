f6b3a6ce01661241c4e78900928812a922cf62e4
Emris666 <46373065+Emris666@users.noreply.github.com>
2019-02-19 19:17:43 +0500

    Sending
    Enter your message

---
+++ input.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8">
+</head>
+<body>
+<form name = "test" method = "POST" action = "input.php">
+<p> Ваше имя:
+<input type = "text" size = "40">
+</p>
+<textarea name = "cooments" cols ="40" rows = "3">  </textarea>
+<input type = "reset" value = "Отправить">
+</form>
+</body>
+</html>
